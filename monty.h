a7871cf0e19820f3117543b8ad72e5f5b553f1e3
Alvinjasper <alvinjspr@gmail.com>
2023-01-18 14:21:05 -0800

    monty

--- monty.h
+++ monty.h
@@ -1,19 +1,23 @@
-#ifndef MONTY_H
-#define MONTY_H
+#ifndef FILE_MONTY
+#define FILE_MONTY
+#define _GNU_SOURCE
+
+/* imports */
 #include <stdio.h>
+#include <ctype.h>
+#include <fcntl.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <sys/types.h>
+#include <stdbool.h>
 #include <sys/stat.h>
-#include <fcntl.h>
-#include <ctype.h>
+#include <sys/types.h>
+
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
  * @prev: points to the previous element of the stack (or queue)
  * @next: points to the next element of the stack (or queue)
- *
  * Description: doubly linked list node structure
  * for stack, queues, LIFO, FIFO Holberton project
  */
@@ -28,7 +32,6 @@ typedef struct stack_s
  * struct instruction_s - opcode and its function
  * @opcode: the opcode
  * @f: function to handle the opcode
- *
  * Description: opcode and its function
  * for stack, queues, LIFO, FIFO Holberton project
  */
@@ -39,46 +42,37 @@ typedef struct instruction_s
 } instruction_t;
 
 /**
- * struct global_variables - all global variables to initialize
- * @order: Stablish the order of the stack, when is a queue or stack
- * @fd: File wil be open
- * @cline: Counter of lines into file
- * @buffer: this pointer allocate the data of each line of the file
- * @stack: Reference to the stack will be added
- * @head: Reference the head of doubly linked list
- * Description: keeps all variables expecting for a value
+ * struct strct_s - struct of info's structs
+ * @file: monty file
+ * @line: line file
+ * @stack: stack_t list
+ * @line_number: line number
+ * @state: int 0
+ * Description: global struct with program info
  */
-typedef struct global_variables
+typedef struct strct_s
 {
-	int order;
-	FILE *fd;
-	unsigned int cline;
-	char *buffer;
-	char *stack;
-	stack_t *head;
-} global_t;
+	FILE *file;
+	char *line;
+	stack_t *stack;
+	unsigned int line_number;
+	int state;
+} strct_t;
+extern strct_t strct;
 
-extern global_t vars;
-void (*get_opcode_func(char *s))(stack_t **head, unsigned int line_number);
-stack_t *add_dnodeint(stack_t **head, const int n);
-stack_t *add_dnodeint_end(stack_t **head, const int n);
-void free_vars(void);
-void free_stack(stack_t *head);
-void stack_push(stack_t **head, unsigned int line_num);
-void stack_pall(stack_t **head, unsigned int line_num);
-void stack_pint(stack_t **head, unsigned int line_num);
-void stack_pop(stack_t **head, unsigned int line_num);
-void stack_add(stack_t **head, unsigned int line_num);
-void stack_swap(stack_t **head, unsigned int line_num);
-void stack_nop(stack_t **head, unsigned int line_num);
-void stack_sub(stack_t **head, unsigned int line_num);
-void stack_div(stack_t **head, unsigned int line_num);
-void stack_mul(stack_t **head, unsigned int line_num);
-void stack_mod(stack_t **head, unsigned int line_num);
-void stack_pchar(stack_t **head, unsigned int line_num);
-void stack_pstr(stack_t **head, unsigned int line_num);
-void stack_queue(stack_t **head, unsigned int line_num);
-void order_stack(stack_t **head, unsigned int line_num);
-FILE *check_open(int argc, char **argv);
-void init(FILE *fd);
-#endif /* #ifndef MONTY_H */
+/* prototypes */
+int _isdigit(char *str);
+void free_stck(stack_t *head);
+strct_t free_collect(strct_t strct);
+strct_t push(char *value, strct_t strct);
+strct_t push1(char *value, strct_t strct);
+void pall(stack_t **stack, unsigned int line_number);
+void nop(stack_t **stack, unsigned int line_number);
+void swap(stack_t **stack, unsigned int line_number);
+void pop(stack_t **stack, unsigned int line_number);
+void pint(stack_t **stack, unsigned int line_number);
+void add(stack_t **stack, unsigned int line_number);
+void sub(stack_t **stack, unsigned int line_number);
+void lst_opcode(stack_t **stack, char *opcode, strct_t strct);
+
+#endif
